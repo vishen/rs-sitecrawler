60213cca2784fc63cc9905d66d698134d9c9e7b4
Jonathan Pentecost <jonathan@karhoo.com>
2017-07-16 13:15:53 +0200

    Can get links for valid html pages

--- src/main.rs
+++ src/main.rs
@@ -18,152 +18,134 @@ use hyper_tls::HttpsConnector;
         - Semantically, it means "While this may or may not have a value, I'm saying it does. If it doesn't, just crash, I don't want to handle the error."
 */
 
+struct Parser {
+    pos: usize,
+    input: String,
+}
+
+impl Parser {
+
+    fn new(input: String) -> Parser {
+        Parser {
+            pos: 0,
+            input: input,
+        }
+    }
+
+    // Read the current character without consuming it.
+    fn peek_char(&mut self) -> char {
+        self.input[self.pos..].chars().next().unwrap()
+    }
+
+    fn next_char(&mut self) -> char {
+        // Increment to pos by the number of bytes in the codepoint
+        // TODO: How does this work?
+        let mut iter = self.input[self.pos..].char_indices();
+        let (_, cur_char) = iter.next().unwrap();
+        let (next_pos, _) = iter.next().unwrap_or((1, ' '));
+        self.pos += next_pos;
+
+        let lower_char = cur_char.to_lowercase().last().unwrap();
+        lower_char
+    }
+
+    fn consume_until<F>(&mut self, test: F) -> String
+        where F: Fn(char) -> bool {
+    // fn consume_until(&mut self, c: char) -> String {
+        let mut result = String::new();
+        while !self.finished() && !test(self.peek_char()) {
+            result.push(self.next_char());
+        }
+
+        result
+    }
+
+    fn consume_whitespaces(&mut self) {
+        let test = |c| {
+            match c {
+                ' ' | '\t' | '\n' => false,
+                _ => true
+            }
+        };
+        self.consume_until(test);
+    }
+
+    fn finished(&self) -> bool {
+        self.pos >= self.input.len()
+    }
+}
+
 fn parse_html() {
     let html = String::from("<HTML><HEAD><meta http-equiv=\"content-type\" content=\"text/html;charset=utf-8\">
 <TITLE src=\"hello\">302 Moved</TITLE></HEAD><BODY>
 <H1>302 Moved</H1>
 The document has moved 忠犬ハチ公
 <A HREF=\"https://www.google.co.uk/?gfe_rd=cr&amp;ei=PpxeWa34NcGN8QeJzIHYAg\">here</A>.
+<a hrefff=None>
+<a href='one world'>
+<a href = /this_is_valid?>
+<a href         = \"hello_world\">
 </BODY></HTML>");
+
+    let mut p = Parser::new(html);
 
     let url_attributes = vec!["href", "src"];
 
-    let length = html.len();
-    let mut pos = 0;
-
-    let mut next_char = move | | {
-        // Increment to pos by the number of bytes in the codepoint
-        let mut iter = html[pos..].char_indices();
-        let (_, cur_char) = iter.next().unwrap();
-        let (next_pos, _) = iter.next().unwrap_or((1, ' '));
-        pos += next_pos;
-        return (pos, cur_char);
-    };
-
     loop {
-        let (mut pos, mut cur_char) = next_char();
-        let mut cur_char_lower = cur_char.to_lowercase().last().unwrap();
+        let mut cur_char = p.next_char();
 
+        // NOTE: This only works because neither attribute starts with the same letter
         for a in url_attributes.clone() {
             let mut score = a.len();
             for ch in a.chars() {
-                if ch == cur_char_lower {
+                if ch == cur_char {
                     score -= 1
                 } else {
                     break;
                 }
+                if score == 0 {
+                    // Check to see that the next char is a valid one of the end of an attrbute
+                    p.consume_whitespaces();
 
-                // TODO(): Why can't I immediately store the result in the existing variables
-                let (npos, ncur_char) = next_char();
-                pos = npos;
-                cur_char = ncur_char;
-                cur_char_lower = cur_char.to_lowercase().last().unwrap();
-           }
-           if score == 0 {
-                println!("SCOREEEEE {}, {}", a, pos)
+                    // Invalid attribute
+                    if p.next_char() != '=' {
+                        break;
+                    }
 
-                // Eat up until a quote or alpha character?
+                    p.consume_whitespaces();
 
-                loop {
-                    let (npos, ncur_char) = next_char();
-                    pos = npos;
-                    cur_char = ncur_char;
-                }
+                    let link = match p.peek_char() {
+                        '"' | '\'' => {
+                            let nc = p.next_char();
+                            let test = |c| c == nc;
+
+                            p.consume_until(test)
+                        },
+                        _ => {
+
+                            let test = |c| {
+                                match c {
+                                    '>' | ' ' | '\t' | '\n' => true,
+                                    _ => false
+                                }
+                            };
+
+                            p.consume_until(test)
+                        }
+                    };
+
+                    println!("link={}", link);
+               } else {
+                    cur_char = p.next_char();
+               }
            }
 
         }
 
-        if pos >= length { break }
+        if p.finished() { break }
     }
 
 }
-
-/*fn parse_html() {
-    let html = String::from("<HTML><HEAD><meta http-equiv=\"content-type\" content=\"text/html;charset=utf-8\">
-<TITLE>302 Moved</TITLE></HEAD><BODY>
-<H1>302 Moved</H1>
-The document has moved 忠犬ハチ公
-<A HREF=\"https://www.google.co.uk/?gfe_rd=cr&amp;ei=PpxeWa34NcGN8QeJzIHYAg\">here</A>.
-</BODY></HTML>");
-
-    let length = html.char_indices().count();
-    let mut html_chars = html.char_indices();
-
-    print!("Length={}\n", length);
-/*
-    let mut value_from_iterator = move |is_alpha: bool| {
-        loop {
-            let k = html_chars.next();
-            match k {
-                None => return (0, ' ', false),
-                _ => {}
-            }
-            let (i, c): (usize, char) = k.unwrap();
-            if is_alpha && !c.is_alphabetic() { continue }
-            return (i, c, true);
-        }
-    };
-
-    'main: loop {
-
-        let (i, c, ok) = value_from_iterator(true);
-        if !ok {
-            break;
-        }
-
-        if c == '<' {
-
-            let (i, c, ok) = value_from_iterator(true);
-            if !ok {
-                break;
-            }
-
-            if c == 'a' || c == 'A' {
-                loop {
-                    let (i, c, ok) = value_from_iterator(false);
-                    if !ok {
-                        break 'main;
-                    }
-                    println!("{}={}", i, c);
-               }
-            }
-        }
-
-    } */
-
-    /*
-    let mut in_quotes = false;
-    let mut current_quote_type = ' ';
-    let mut eating = false;
-    let mut last_char = ' ';
-    let mut in_decl = false;
-
-    for c in html.chars() {
-
-        if c == '<' {
-            in_decl = true
-        } else if in_decl
-                && (c == 'a' || c == 'A')
-                && (last_char == ' ' || last_char == '<') {
-            eating = true
-        } else if eating && !in_quotes && c == '>' {
-            eating = false
-        } else if eating {
-            if !in_quotes && (c == '"' || c == '\''){
-                current_quote_type = c;
-                in_quotes = true;
-            } else if in_quotes && c == current_quote_type {
-                in_quotes = false;
-            } else if in_quotes {
-                print!("char: {}\n", c);
-            }
-        }
-
-        last_char = c;
-
-
-    }*/
-}*/
 
 fn main() {
 
