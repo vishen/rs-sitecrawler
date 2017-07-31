e23ad16393939d3e038b7ab272ecff7475f6a933
Jonathan Pentecost <jonathan@karhoo.com>
2017-07-31 17:57:26 +0300

    Added hashmap for links -> frequency

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ $ cargo run
 
 ## Useful Resources
 ```
+- http://blog.skylight.io/rust-means-never-having-to-close-a-socket/
 - https://docs.rs/hyper/0.11.1/hyper/  # Hyper documentation
 - https://doc.rust-lang.org/std/  # Standard library documentation for Rust
 - https://doc.rust-lang.org/book/second-edition  # Basic tutorial
--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,13 @@ extern crate hyper;
 extern crate tokio_core;
 extern crate hyper_tls;
 
-use std::io::{self, Write};
+use std::collections::HashMap;
 
-use futures::Future;
-use futures::future;
+use futures::{future, Future};
 use futures::stream::Stream;
 
 use hyper::{Client, Error};
 use hyper_tls::HttpsConnector;
-
-/*
-    - unwrap():
-        - https://github.com/rust-lang/rust/blob/09e2ad13d0aa01143bcb20dece3ff6c5a7e34ea3/src/libcore/option.rs#L329-L359
-        - Semantically, it means "While this may or may not have a value, I'm saying it does. If it doesn't, just crash, I don't want to handle the error."
-*/
 
 struct Parser {
     pos: usize,
@@ -51,7 +44,6 @@ impl Parser {
 
     fn consume_until<F>(&mut self, test: F) -> String
         where F: Fn(char) -> bool {
-    // fn consume_until(&mut self, c: char) -> String {
         let mut result = String::new();
         while !self.finished() && !test(self.peek_char()) {
             result.push(self.next_char());
@@ -75,21 +67,12 @@ impl Parser {
     }
 }
 
-fn parse_html() {
-    let html = String::from("<HTML><HEAD><meta http-equiv=\"content-type\" content=\"text/html;charset=utf-8\">
-<TITLE src=\"hello\">302 Moved</TITLE></HEAD><BODY>
-<H1>302 Moved</H1>
-The document has moved 忠犬ハチ公
-<A HREF=\"https://www.google.co.uk/?gfe_rd=cr&amp;ei=PpxeWa34NcGN8QeJzIHYAg\">here</A>.
-<a hrefff=None>
-<a href='one world'>
-<a href = /this_is_valid?>
-<a href = extra123123?=hello extra>
-<a href         = \"hello_world\">
-</BODY></HTML>");
+fn parse_html(html: String) -> HashMap<String, u32> {
 
+    // TODO(): Ignore `url_attributes` in `<script>` tags
+
+    let mut found_links: HashMap<String, u32> = HashMap::new();
     let mut p = Parser::new(html);
-
     let url_attributes = vec!["href", "src"];
 
     loop {
@@ -134,8 +117,10 @@ The document has moved 忠犬ハチ公
                             p.consume_until(test)
                         }
                     };
+                    // println!("link={}", link);
 
-                    println!("link={}", link);
+                    // NOTE: Can't use `link` after this as `found_links.entry` now owns it
+                    *found_links.entry(link).or_insert(0) += 1;
                } else {
                     cur_char = p.next_char();
                }
@@ -146,36 +131,44 @@ The document has moved 忠犬ハチ公
         if p.finished() { break }
     }
 
+    // println!("{:?}", found_links);
+
+    found_links
+
 }
 
 fn main() {
 
-    parse_html();
+/*    let html = String::from("<HTML><HEAD><meta http-equiv=\"content-type\" content=\"text/html;charset=utf-8\">
+<TITLE src=\"hello\">302 Moved</TITLE></HEAD><BODY>
+<H1 href=hello_world>302 Moved</H1>
+The document has moved 忠犬ハチ公 href=oneoneone
+src=&quot;//www.redditstatic.com/video-settings.svg&quot;&gt;
+<A HREF=\"https://www.google.co.uk/?gfe_rd=cr&amp;ei=PpxeWa34NcGN8QeJzIHYAg\">here</A>.
+<a hrefff=None>
+<a href='one world'>
+<a href = /this_is_valid?>
+<a href = extra123123?=hello extra>
+<a href         = \"hello_world\">
+</BODY></HTML>");
+    parse_html(html);
     return;
+*/
 
-
-    let url = "https://google.com".parse::<hyper::Uri>().unwrap();
+    let url = "https://www.reddit.com/".parse::<hyper::Uri>().unwrap();
 
     let mut core = tokio_core::reactor::Core::new().unwrap();
     let handle = core.handle();
 
     let client = Client::configure()
-                        .connector(HttpsConnector::new(4, &handle).unwrap())
-                        .build(&handle);
+        .connector(HttpsConnector::new(4, &handle).unwrap())
+        .build(&handle);
 
     // client.get -> hyper::client::FutureResponse
     // .and_then -> Execute another future after this one has resolved successfully
     let work = client.get(url).and_then(|res| {
-        println!("Response: {}", res.status());
-        println!("Headers: \n{}", res.headers());
-
-        /*
-        // .for_each() -> Runs this stream to completion, executing the provided closure for each element on the stream.
-        res.body().for_each(|chunk| {
-            io::stdout().write_all(&chunk).map_err(From::from)
-
-        })
-        */
+        // println!("Response: {}", res.status());
+        // println!("Headers: \n{}", res.headers());
 
         res.body().fold(Vec::new(), |mut v, chunk| {
             v.extend(&chunk[..]);
@@ -183,6 +176,12 @@ fn main() {
         }).and_then(|chunks| {
             let s = String::from_utf8(chunks).unwrap();
             print!("BODY: {}\n", s);
+
+            let links = parse_html(s);
+            for (link, count) in &links {
+                println!("({}) -> {}", count, link);
+            }
+
             future::ok::<_, Error>(0)
         })
 
